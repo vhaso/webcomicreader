6745629dbdde91d350ca34301de9e014e0cc6513
Klaas-Jan <k-j.gorter@outlook.com>
2020-09-06 16:25:23 -0500

    Add a helper function for splitting a pdf into pngs

---
+++ helpers/pdf_to_pages.py
@@ -0,0 +1,30 @@
+import fitz
+import os
+import shutil
+import sys
+from PIL import Image
+
+def iter_pages(reader):
+    for page_num in range(reader.numPages):
+        yield reader.getPage(page_num)
+
+if __name__ == '__main__':
+    pdf_path = sys.argv[1]
+    destination = sys.argv[2]
+    if destination in os.listdir():
+        shutil.rmtree(destination)
+    os.mkdir(destination)
+
+    doc = fitz.open(pdf_path)
+    for i in range(len(doc)):
+        path = os.path.join(destination, f"{i}.png")
+        for img in doc.getPageImageList(i):
+            xref = img[0]
+            pix = fitz.Pixmap(doc, xref)
+            if pix.n < 5:       # this is GRAY or RGB
+                pix.writePNG(path)
+            else:               # CMYK: convert to RGB first
+                pix1 = fitz.Pixmap(fitz.csRGB, pix)
+                pix1.writePNG(path)
+                pix1 = None
+            pix = None
